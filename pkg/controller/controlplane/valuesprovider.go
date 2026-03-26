2477a933ad880ebdfe641f94db862f2fe0b4770c
Jan Steffen <Jan.Steffen@stackit.cloud>
2026-03-26 11:11:40 +0100

    feat(webhook): set fixed port and update webhook URL with seed name

--- pkg/controller/controlplane/valuesprovider.go
+++ pkg/controller/controlplane/valuesprovider.go
@@ -1081,7 +1081,7 @@ func (vp *valuesProvider) getControlPlaneShootChartValues(ctx context.Context, c
 		return nil, err
 	}
 
-	podIdentityWebhook, err := vp.getPodIdentityWebhookShootChartValues(secretsReader)
+	podIdentityWebhook, err := vp.getPodIdentityWebhookShootChartValues(cluster, secretsReader)
 	if err != nil {
 		return nil, err
 	}
@@ -1317,6 +1317,7 @@ func getPodIdentityWebhookChartValues(
 }
 
 func (vp *valuesProvider) getPodIdentityWebhookShootChartValues(
+	cluster *extensionscontroller.Cluster,
 	secretsReader secretsmanager.Reader,
 ) (map[string]any, error) {
 	caSecret, found := secretsReader.Get(caNameControlPlane)
@@ -1332,6 +1333,7 @@ func (vp *valuesProvider) getPodIdentityWebhookShootChartValues(
 	return map[string]any{
 		"webhook": map[string]any{
 			"caBundle": caBundle,
+			"seedName": cluster.Seed.ObjectMeta.Name,
 		},
 	}, nil
 }
