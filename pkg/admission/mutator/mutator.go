5ce82567fff828e0f09453a9439e942788e644e4
Maximilian Geberl <maximilian.geberl@digits.schwarz>
2026-04-30 08:48:33 +0200

    Remove not needed Provider from webhook args

--- pkg/admission/mutator/mutator.go
+++ pkg/admission/mutator/mutator.go
@@ -26,9 +26,8 @@ func New(mgr manager.Manager) (*extensionswebhook.Webhook, error) {
 	logger.Info("Setting up webhook", "name", Name)
 
 	return extensionswebhook.New(mgr, extensionswebhook.Args{
-		Provider: stackit.Type,
-		Name:     Name,
-		Path:     "/webhooks/mutate",
+		Name: Name,
+		Path: "/webhooks/mutate",
 		Mutators: map[extensionswebhook.Mutator][]extensionswebhook.Type{
 			NewShootMutator(mgr):                  {{Obj: &gardencorev1beta1.Shoot{}}},
 			NewNamespacedCloudProfileMutator(mgr): {{Obj: &gardencorev1beta1.NamespacedCloudProfile{}, Subresource: new("status")}},
