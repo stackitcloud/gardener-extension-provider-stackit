c10cf8c25478af7e6601717a760ffeab24ecc972
Niclas Schad <niclas.schad@stackit.cloud>
2026-06-01 14:41:29 +0200

    use proper example
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>

--- pkg/controller/bastion/options_test.go
+++ pkg/controller/bastion/options_test.go
@@ -9,7 +9,6 @@ import (
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	stackitclient "github.com/stackitcloud/gardener-extension-provider-stackit/v2/pkg/stackit/client"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -250,16 +249,18 @@ var _ = Describe("Options", func() {
 			WorkerSecurityGroupID: "security-group-id-nodes",
 		}))
 	})
-	It("should correctly generate a valid bastion name", func() {
-		shoot.Status.TechnicalID = "something-very-very-very-very-very-very-very-very-long"
-		bastionName := stackitclient.BuildResourceName(shoot.Status.TechnicalID, "-bastion-", bastion.Name)
-		Expect(a.DetermineOptions(ctx, bastion, cluster, projectID)).To(Equal(&Options{
+	It("should correctly generate a valid bastion name if length is more than 63", func() {
+		shoot.Status.TechnicalID = "shoot--982656e3f6--e2e-6oy241v"
+		bastion.Name = "e2e-6oy241v-private-cluster"
+		result, err := a.DetermineOptions(ctx, bastion, cluster, projectID)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(result).To(Equal(&Options{
 			Bastion:      bastion,
 			ProjectID:    projectID,
-			ResourceName: bastionName,
+			ResourceName: "shoot--982656e3f6--e2e-6oy241v-bastion-e2e-6oy241v-pri-861f41cc",
 			Labels: map[string]string{
-				"kubernetes.io/cluster": "something-very-very-very-very-very-very-very-very-long",
-				"kubernetes.io/bastion": "foo",
+				"kubernetes.io/cluster": "shoot--982656e3f6--e2e-6oy241v",
+				"kubernetes.io/bastion": "e2e-6oy241v-private-cluster",
 			},
 			Region:                "eu01",
 			AvailabilityZone:      "eu01-1",
@@ -268,6 +269,7 @@ var _ = Describe("Options", func() {
 			NetworkID:             "network-id",
 			WorkerSecurityGroupID: "security-group-id-nodes",
 		}))
+		Expect(result.ResourceName).To(HaveLen(63))
 	})
 
 	DescribeTable("customLabelDomain for bastion labels",
