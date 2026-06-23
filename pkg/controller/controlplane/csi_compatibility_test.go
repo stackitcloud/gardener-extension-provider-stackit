6326c1b0ea9f9309e7e47e076d4c5843a5cf27b4
Florian Thienel <florian.thienel@inovex.de>
2026-06-23 13:07:43 +0200

    add a unit test scaffolding

---
+++ pkg/controller/controlplane/csi_compatibility_test.go
@@ -0,0 +1,234 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package controlplane
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+
+	resourcesv1alpha1 "github.com/gardener/gardener/pkg/apis/resources/v1alpha1"
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	stackitv1alpha1 "github.com/stackitcloud/gardener-extension-provider-stackit/v2/pkg/apis/stackit/v1alpha1"
+	"github.com/stackitcloud/gardener-extension-provider-stackit/v2/pkg/openstack"
+)
+
+type mockRoundTripper struct{}
+
+func (m *mockRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	var body string
+	switch req.URL.Path {
+	case "/version":
+		body = `{"major":"1","minor":"29","gitVersion":"v1.29.0"}`
+	case "/api":
+		body = `{"kind":"APIVersions","versions":["v1"]}`
+	case "/apis":
+		body = `{"kind":"APIGroupList","groups":[]}`
+	default:
+		body = `{"kind":"Status","status":"Failure","message":"Not Found","reason":"NotFound","code":404}`
+	}
+
+	return &http.Response{
+		StatusCode: http.StatusOK,
+		Header:     http.Header{"Content-Type": []string{"application/json"}},
+		Body:       io.NopCloser(bytes.NewReader([]byte(body))),
+	}, nil
+}
+
+var _ = Describe("CompatCSICompatibilityHandler", func() {
+	var (
+		ctx        context.Context
+		fakeClient client.Client
+		handler    *CompatCSICompatibilityHandler
+		namespace  string
+		config     *rest.Config
+	)
+
+	BeforeEach(func() {
+		ctx = context.Background()
+		namespace = "test-namespace"
+
+		fakeClient = fakeclient.NewClientBuilder().
+			WithScheme(kubernetes.SeedScheme).
+			Build()
+
+		config = &rest.Config{
+			Host:      "https://localhost",
+			Transport: &mockRoundTripper{},
+		}
+
+		handler = NewCompatCSICompatibilityHandler(fakeClient, config)
+	})
+
+	Describe("#HandleSeedCSICompatibility", func() {
+		Context("when CSICompatibilityMode is DEFAULT", func() {
+			It("should delete the managed resource", func() {
+				cpConfig := &stackitv1alpha1.ControlPlaneConfig{
+					Storage: &stackitv1alpha1.Storage{
+						CSI: &stackitv1alpha1.CSI{
+							Name: string(stackitv1alpha1.DEFAULT),
+						},
+					},
+				}
+
+				// Create the managed resource and secret beforehand to ensure deletion works
+				mr := &resourcesv1alpha1.ManagedResource{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "stackit-csi-compat-chart",
+						Namespace: namespace,
+					},
+				}
+				secret := &corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "managedresource-stackit-csi-compat-chart",
+						Namespace: namespace,
+					},
+				}
+				Expect(fakeClient.Create(ctx, mr)).To(Succeed())
+				Expect(fakeClient.Create(ctx, secret)).To(Succeed())
+
+				err := handler.HandleSeedCSICompatibility(ctx, namespace, cpConfig, nil)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Check deletion
+				err = fakeClient.Get(ctx, types.NamespacedName{Name: "stackit-csi-compat-chart", Namespace: namespace}, mr)
+				Expect(client.IgnoreNotFound(err)).To(Succeed())
+				Expect(err).ToNot(Succeed())
+			})
+		})
+
+		Context("when CSICompatibilityMode is COMPAT", func() {
+			It("should deploy the seed csi compatibility mode", func() {
+				cpConfig := &stackitv1alpha1.ControlPlaneConfig{
+					Storage: &stackitv1alpha1.Storage{
+						CSI: &stackitv1alpha1.CSI{
+							Name: string(stackitv1alpha1.COMPAT),
+						},
+					},
+				}
+
+				controlPlaneValues := map[string]any{
+					"global": map[string]any{
+						"genericTokenKubeconfigSecretName": "generic-token-kubeconfig-92e9ae14",
+					},
+					openstack.CSISTACKITControllerName: map[string]any{
+						"foo": "bar",
+					},
+				}
+
+				err := handler.HandleSeedCSICompatibility(ctx, namespace, cpConfig, controlPlaneValues)
+				Expect(err).NotTo(HaveOccurred())
+
+				mr := &resourcesv1alpha1.ManagedResource{}
+				err = fakeClient.Get(ctx, types.NamespacedName{Name: "stackit-csi-compat-chart", Namespace: namespace}, mr)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("#HandleShootCSICompatibility", func() {
+		Context("when CSICompatibilityMode is DEFAULT", func() {
+			It("should delete the managed resource", func() {
+				cpConfig := &stackitv1alpha1.ControlPlaneConfig{
+					Storage: &stackitv1alpha1.Storage{
+						CSI: &stackitv1alpha1.CSI{
+							Name: string(stackitv1alpha1.DEFAULT),
+						},
+					},
+				}
+
+				// Create the managed resource and secret beforehand to ensure deletion works
+				mr := &resourcesv1alpha1.ManagedResource{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "stackit-csi-compat-shoot-chart",
+						Namespace: namespace,
+					},
+				}
+				secret := &corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "managedresource-stackit-csi-compat-shoot-chart",
+						Namespace: namespace,
+					},
+				}
+				Expect(fakeClient.Create(ctx, mr)).To(Succeed())
+				Expect(fakeClient.Create(ctx, secret)).To(Succeed())
+
+				err := handler.HandleShootCSICompatibility(ctx, namespace, cpConfig, nil)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Check deletion
+				err = fakeClient.Get(ctx, types.NamespacedName{Name: "stackit-csi-compat-shoot-chart", Namespace: namespace}, mr)
+				Expect(client.IgnoreNotFound(err)).To(Succeed())
+				Expect(err).ToNot(Succeed())
+			})
+		})
+
+		Context("when CSICompatibilityMode is COMPAT", func() {
+			It("should deploy the shoot csi compatibility mode with blockLegacyCreation = false", func() {
+				cpConfig := &stackitv1alpha1.ControlPlaneConfig{
+					Storage: &stackitv1alpha1.Storage{
+						CSI: &stackitv1alpha1.CSI{
+							Name: string(stackitv1alpha1.COMPAT),
+						},
+					},
+				}
+
+				values := map[string]any{
+					"global": map[string]any{
+						"genericTokenKubeconfigSecretName": "generic-token-kubeconfig-92e9ae14",
+					},
+					openstack.CSISTACKITControllerName: map[string]any{
+						"foo": "bar",
+					},
+				}
+
+				err := handler.HandleShootCSICompatibility(ctx, namespace, cpConfig, values)
+				Expect(err).NotTo(HaveOccurred())
+
+				mr := &resourcesv1alpha1.ManagedResource{}
+				err = fakeClient.Get(ctx, types.NamespacedName{Name: "stackit-csi-compat-shoot-chart", Namespace: namespace}, mr)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+
+		Context("when CSICompatibilityMode is COMPATBLOCK", func() {
+			It("should deploy the shoot csi compatibility mode with blockLegacyCreation = true", func() {
+				cpConfig := &stackitv1alpha1.ControlPlaneConfig{
+					Storage: &stackitv1alpha1.Storage{
+						CSI: &stackitv1alpha1.CSI{
+							Name: string(stackitv1alpha1.COMPATBLOCK),
+						},
+					},
+				}
+
+				values := map[string]any{
+					"global": map[string]any{
+						"genericTokenKubeconfigSecretName": "generic-token-kubeconfig-92e9ae14",
+					},
+					openstack.CSISTACKITControllerName: map[string]any{
+						"foo": "bar",
+					},
+				}
+
+				err := handler.HandleShootCSICompatibility(ctx, namespace, cpConfig, values)
+				Expect(err).NotTo(HaveOccurred())
+
+				mr := &resourcesv1alpha1.ManagedResource{}
+				err = fakeClient.Get(ctx, types.NamespacedName{Name: "stackit-csi-compat-shoot-chart", Namespace: namespace}, mr)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+	})
+})
