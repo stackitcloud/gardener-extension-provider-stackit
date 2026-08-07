088eecc4a6221fc15de558bb37dde8228c07f290
Niclas Schad <niclas.schad@stackit.cloud>
2026-08-07 13:13:46 +0200

    make format
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>

--- pkg/admission/mutator/shoot_test.go
+++ pkg/admission/mutator/shoot_test.go
@@ -11,9 +11,10 @@ import (
 	. "github.com/gardener/gardener/pkg/utils/test/matchers"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/stackitcloud/gardener-extension-provider-stackit/v2/pkg/stackit"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/stackitcloud/gardener-extension-provider-stackit/v2/pkg/stackit"
 )
 
 var _ = Describe("Shoot mutator", func() {
