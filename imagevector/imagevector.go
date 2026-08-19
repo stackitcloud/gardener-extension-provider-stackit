5e272cad6d81b7bb64a26c7beece86114c7113f5
Felix Breuer <f.breuer94@gmail.com>
2026-08-19 16:39:25 +0200

    add caBundle to imagevector
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>

--- imagevector/imagevector.go
+++ imagevector/imagevector.go
@@ -17,19 +17,26 @@ var (
 	imagesYAML string
 	// ImageVector is the image vector that contains all the needed images.
 	imageVector imagevector.ImageVector
+	// caBundle contains the optional registry CA bundle from the image vector overwrite.
+	caBundle *imagevector.CABundle
 )
 
 func init() {
 	var err error
 
-	imageVector, err = imagevector.Read([]byte(imagesYAML))
+	imageVector, caBundle, err = imagevector.Read([]byte(imagesYAML))
 	runtime.Must(err)
 
-	imageVector, err = imagevector.WithEnvOverride(imageVector, imagevector.OverrideEnv)
+	imageVector, caBundle, err = imagevector.WithEnvOverride(imageVector, caBundle, imagevector.OverrideEnv)
 	runtime.Must(err)
 }
 
 // ImageVector is the image vector that contains all the needed images.
 func ImageVector() imagevector.ImageVector {
 	return imageVector
+}
+
+// CABundle contains the optional registry CA bundle from the image vector configuration.
+func CABundle() *imagevector.CABundle {
+	return caBundle
 }
