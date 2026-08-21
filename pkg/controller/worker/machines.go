9a4691414df5243360087dff4564aa67a09b9f66
Niclas Schad <niclas.schad@stackit.cloud>
2026-08-21 14:55:32 +0200

    remove comment; this case is covered by the Normalize function
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>

--- pkg/controller/worker/machines.go
+++ pkg/controller/worker/machines.go
@@ -111,7 +111,6 @@ func (w *workerDelegate) generateMachineConfig(ctx context.Context) error {
 		// nolint:gosec // check above ensures no overflow can occur
 		zoneLen := int32(len(pool.Zones))
 
-		// TODO: this returns a capability with arch amd64, but worker pool defined architecture arm64.
 		machineTypeFromCloudProfile := gardencorev1beta1helper.FindMachineTypeByName(w.cluster.CloudProfile.Spec.MachineTypes, pool.MachineType)
 		if machineTypeFromCloudProfile == nil {
 			return fmt.Errorf("machine type %q not found in cloud profile %q", pool.MachineType, w.cluster.CloudProfile.Name)
